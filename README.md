03c8d76aacf6709e10fb5ce528ed6129733d10ae
pezzaliapp <pezzalialessandro@gmail.com>
2025-04-12 09:59:19 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # DamAPP
-Gioco della Dama contro l'IA
+
+PWA open source che simula il gioco della dama italiana.
+
+## Funzioni attuali
+- Tabellone 8x8
+- Pedine rosse e nere
+- Algoritmo AI avanzato con Minimax + Alpha-Beta
+- Profondità dinamica
+
+## Autore
+Alessandro Pezzali – [pezzaliAPP.com](https://www.pezzaliapp.com)
---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('damapp-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/manifest.json',
+        '/icon/DamAPP-192.png',
+        '/icon/DamAPP-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
